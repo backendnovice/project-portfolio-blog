b9f6a0182beed4060df0ade955892c205a652ed3
ungpung0 <ungpung0@gmail.com>
2023-03-12 11:33:06 +0900

    :tada: Create Spring Project
    Create Spring Project via Spring initializr.

---
+++ src/test/java/com/backendnovice/projectportfolioblog/ProjectPortfolioBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendnovice.projectportfolioblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPortfolioBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
